41e66ee194fcb13152f4a8871e204a305247ac45
Tyler Rick <tyler@tylerrick.com>
2019-11-19 08:39:38 -0800

    Allow setting the host via PGHOST
    This is consistent with how we can set port with PGPORT, and necessary because some people connect
    to a different host (such as postgres running in a container).

--- spec/support/connection.rb
+++ spec/support/connection.rb
@@ -3,6 +3,8 @@ require "active_record"
 def db_config
   {
     adapter: "postgresql",
+    host:     ENV.fetch("PGHOST", "localhost"),
+    port:     ENV.fetch("PGPORT", "5432"),
     database: ENV.fetch("TEST_DATABASE", "pg_enum_test"),
     username: ENV.fetch("TEST_USER") { ENV.fetch("USER", "pg_enum") },
     password: ENV["TEST_PASSWORD"]
