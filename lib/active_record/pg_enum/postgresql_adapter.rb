3dbd9a02642256eb746bbe327c9d5052c37f93fe
Aaron Tavistock <aaron@geekshow.com>
2020-11-25 13:02:04 -0800

    Convert postgres adapter code to use Arel, add two helpers for cleaner enum use in ActiveRecord

--- lib/active_record/pg_enum/postgresql_adapter.rb
+++ lib/active_record/pg_enum/postgresql_adapter.rb
@@ -13,29 +13,36 @@ module ActiveRecord
       #
       #   { "foo_type" => ["foo", "bar", "baz"] }
       def enum_types
-        res = exec_query(<<-SQL.strip_heredoc, "SCHEMA")
-          SELECT t.typname AS enum_name, array_agg(e.enumlabel ORDER BY e.enumsortorder) AS enum_value
-          FROM pg_type t
-          JOIN pg_enum e ON t.oid = e.enumtypid
-          JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
-          WHERE n.nspname = ANY (current_schemas(false))
-          GROUP BY enum_name;
-        SQL
+        pg_type = Arel::Table.new(:pg_type)
+        pg_enum = Arel::Table.new(:pg_enum)
+        pg_namespace = Arel::Table.new(:pg_namespace)
+        current_schema = Arel::Nodes::NamedFunction.new('current_schemas', [Arel::Nodes::False.new])
+        any_current_schema = Arel::Nodes::NamedFunction.new('ANY', [current_schema])
 
-        coltype = res.column_types["enum_value"]
-        deserialize = if coltype.respond_to?(:deserialize)
-          proc { |values| coltype.deserialize(values) }
-        elsif coltype.respond_to?(:type_cast_from_database)
-          proc { |values| coltype.type_cast_from_database(values) }
-        else
-          proc { |values| coltype.type_cast(values) }
+        query = pg_type.
+          project(pg_type[:typname], pg_enum[:enumlabel]).
+          join(pg_enum).on(pg_enum[:enumtypid].eq(pg_type[:oid])).
+          join(pg_namespace).on(pg_type[:typnamespace].eq(pg_namespace[:oid])).
+          where(pg_namespace[:nspname].eq(any_current_schema))
+        rows = ActiveRecord::Base.connection.select_rows(query)
+
+        rows.reduce({}) do |accum, (type, value)|
+          accum[type.to_sym] ||= []
+          accum[type.to_sym] << value.to_s
+          accum
         end
+      end
 
-        res.rows
-          .map { |name, values| [name, values] }
-          .sort { |a, b| a.first <=> b.first }
-          .each_with_object({}) { |(name, values), memo| memo[name] = deserialize.call(values) }
+      def enum_values(type_name)
+        enum_types[type_name.to_sym]
+      end
+
+      def postgres_enum(name, type_name = nil)
+        type_name ||= name
+        values = enum_values(type_name)
+        enum name.to_sym => values.zip(values).to_h
       end
     end
+
   end
 end
